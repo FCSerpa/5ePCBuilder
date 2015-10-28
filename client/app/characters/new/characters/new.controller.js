95af3f0b952bac38da15329895737ca93b276680
Francis Serpa <fcserpa@gmail.com>
2015-10-28 12:44:37 -0700

    fixing up new characters page

--- client/app/characters/new/characters/new.controller.js
+++ client/app/characters/new/characters/new.controller.js
@@ -1,6 +1,32 @@
 'use strict';
 
 angular.module('5ePcApp')
-  .controller('CharactersNewCtrl', function ($scope) {
+  .controller('CharactersNewCtrl', function ($scope, Auth) {
     $scope.message = 'Hello';
+    $scope.getCurrentUser = Auth.getCurrentUser;
+    $scope.Math = window.Math;
+
+	$scope.character = {
+		name: '',
+		characterClass: '',
+		level: 1,
+		race: '',
+		background: '',
+		trait: '',
+		ideal: '',
+		bond: '',
+		flaw: '',
+		alignment: '',
+		player: '',
+		xp: 0,
+		str: 8,
+		dex: 8,
+		con: 8,
+		intel: 8,
+		wis: 8,
+		cha: 8,
+		gold: 0,
+		equipment: []
+	};
+
   });
